59fd9513dd9721fd13864603f5ad17e64e1f0375
mwakhare <mwakhare@torinit.com>
2017-03-02 16:40:07 +0530

    This has React.createClass

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,15 @@
+var Greet = React.createClass ({
+  render: function (){
+    return (
+      <div>
+        <h1>This is from React.creatClass</h1>
+        <p>This is simple para</p>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render (
-  <h1>This is react app.jsx</h1>,
+  <Greet/>,
   document.getElementById('app')
 );
