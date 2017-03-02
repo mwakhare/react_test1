b05d8b32ef82369641f2f16d17e147109c940891
mwakhare <mwakhare@torinit.com>
2017-03-02 17:00:52 +0530

    two param passed

--- public/app.jsx
+++ public/app.jsx
@@ -2,23 +2,34 @@ var Greet = React.createClass ({
   getDefaultProps: function () {
     return (
       {
-        name: 'React'
+        name: 'React',
+        message: 'Test Message'
       }
     )
   },
   render: function (){
     var name = this.props.name;
+    var message = this.props.message;
 
     return (
       <div>
         <h1>Hello from {name}</h1>
-        <p>This is simple para</p>
+        <p>{message} </p>
       </div>
     );
   }
 });
 
+
+//var firstName = "Tushar";
+//var msg = "This is sample message from outside";
+// ReactDOM.render (
+//   <Greet name={firstName} message={msg}/>,
+//   document.getElementById ('app')
+// );
+
+
 ReactDOM.render (
-  <Greet />,
-  document.getElementById('app')
+  <Greet/>,
+  document.getElementById ('app')
 );
