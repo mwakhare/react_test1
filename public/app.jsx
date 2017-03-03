7475a82d0569a5174c244871b55585e98113b955
mwakhare <mwakhare@torinit.com>
2017-03-03 10:14:24 +0530

    GreetMsg added.

---
+++ public/app.jsx
@@ -0,0 +1,64 @@
+var GreetMsg = React.createClass ({
+    render: function (){
+      return (
+        <div>
+          <h1>Some heading</h1>
+          <p>some paragraph</p>
+        </div>
+      );
+    }
+});
+
+var Greet = React.createClass ({
+  getDefaultProps: function () {
+    return ({
+        name: 'React',
+        message: 'Test Message'
+      });
+  },
+  getInitialState: function() {
+    return {
+          name:  this.props.name
+      };
+  },
+  onButtonClick:function(e){
+    e.preventDefault ();
+    var name = this.refs.inputName.value;
+
+    this.setState ({
+      name : name
+    });
+
+    alert(name);
+  },
+  render: function (){
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello from {name}</h1>
+        <p>{message} </p>
+        <GreetMsg/>
+        <form onSubmit={this.onButtonClick}>
+          <input type='text' ref='inputName'/>
+          <button>Set Name</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+
+//var firstName = "Tushar";
+//var msg = "This is sample message from outside";
+// ReactDOM.render (
+//   <Greet name={firstName} message={msg}/>,
+//   document.getElementById ('app')
+// );
+
+
+ReactDOM.render (
+  <Greet/>,
+  document.getElementById ('app')
+);
